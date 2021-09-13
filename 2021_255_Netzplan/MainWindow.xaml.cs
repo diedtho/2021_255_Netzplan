22d29c12025707bef38383f087d45439ede6ecc4
DieTho <diederichst@bfwob.de>
2021-09-13 08:28:43 +0200

    Projektdateien hinzufügen.

---
+++ 2021_255_Netzplan/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2021_255_Netzplan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        List<Vorgang> listeVorgaenge = new List<Vorgang>();
+
+        private void btnClickVorgangHinzufuegen(object sender, RoutedEventArgs e)
+        {            
+            VorgangCreate secondWindow = new();
+            secondWindow.ShowDialog();            
+            listeVorgaenge.Add(new Vorgang(secondWindow.tbVorgang.Text, secondWindow.tbBeschreibung.Text,
+                secondWindow.tbDauerInTagen.Text, secondWindow.tbVorgaenger.Text));
+            lvVorgaenge.ItemsSource = listeVorgaenge.ToList();
+        }
+
+        private void mnuitmExit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void mnuitmSpeichern(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 2021_255_Netzplan/Vorgang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_255_Netzplan
+{
+    public class Vorgang
+    {
+        public string vorgangBuchstb { get; set; }
+        public string beschreibung { get; set; }
+        public string dauerInTagen { get; set; }
+        public string vorgaenger { get; set; }
+
+        public Vorgang()
+        {
+
+        }
+        public Vorgang(string vorg, string beschreib, string dauerTage, string vorgaeng)
+        {
+            vorgangBuchstb = vorg;
+            beschreibung = beschreib;
+            dauerInTagen = dauerTage;
+            vorgaenger = vorgaeng;
+        }
+    }
+
+
+}
